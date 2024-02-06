7819c636996a48e96b7aa999b59cf42380547395
Lars Aaberg <lars@aaberg.cc>
2024-02-06 17:28:16 +0100

    Beginning contact details page

--- ContactServiceApp/Contacts/ContactService.cs
+++ ContactServiceApp/Contacts/ContactService.cs
@@ -24,11 +24,13 @@ public class ContactService
 
         var contactGrains = await tenantGrain.ListContacts();
 
+        var contactIds = ArrayPool<string>.Shared.Rent(contactGrains.Length);
         var nameTasks = ArrayPool<Task<string>>.Shared.Rent(contactGrains.Length);
         var phoneTasks = ArrayPool<Task<Phone[]>>.Shared.Rent(contactGrains.Length);
         var emailTasks = ArrayPool<Task<EmailAddress[]>>.Shared.Rent(contactGrains.Length);
         for (var i = 0; i < contactGrains.Length; i++)
         {
+            contactIds[i] = contactGrains[i].GetGrainId().Key.ToString();
             nameTasks[i] = contactGrains[i].GetName();
             phoneTasks[i] = contactGrains[i].ListPhones();
             emailTasks[i] = contactGrains[i].ListEmails();
@@ -42,7 +44,7 @@ public class ContactService
             var phones = await phoneTasks[i];
             var emails = await emailTasks[i];
             
-            contacts.Add(new Contact(userId, name, emails, phones));
+            contacts.Add(new Contact(contactIds[i], name, emails, phones));
         }
 
         return contacts.ToImmutable();
